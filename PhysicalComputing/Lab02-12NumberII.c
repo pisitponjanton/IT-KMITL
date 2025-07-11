d6bd52d21137264bc5fabd9c2a8614f18fb31cb1
pisitponjanton <pisitponjan223@gmail.com>
2025-07-11 22:33:51 +0700

    update

--- PhysicalComputing/Lab02-12NumberII.c
+++ PhysicalComputing/Lab02-12NumberII.c
@@ -4,10 +4,10 @@ int main()
 {
     char num[5];
     scanf("%s", num);
-    printf("%-80.1s", num);
+    printf("%-81.1s", num);
     printf("%-81.2s", num);
-    printf("%-82.3s", num);
-    printf("%-83.4s", num);
-    printf("%-84.5s", num);
+    printf("%-81.3s", num);
+    printf("%-81.4s", num);
+    printf("%.5s", num);
     return 0;
 }
