0eea520d8cb3d8333356f0f5aaf76f1f34168714
pisitponjanton <pisitponjan223@gmail.com>
2025-07-13 21:33:39 +0700

    update

--- PhysicalComputing/Lab02-11Number.c
+++ PhysicalComputing/Lab02-11Number.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
-int main()
-{
-    char num[5];
-    scanf("%s", num);
-    printf("%c%c%c%c%c", num[2],num[3],num[4],num[0],num[1]);
+int main() {
+    char c1, c2, c3, c4, c5;
+
+    if (scanf(" %c %c %c %c %c", &c1, &c2, &c3, &c4, &c5) != 5) {
+        return 1;
+    }
+
+    int d1 = c1 - '0';
+    int d2 = c2 - '0';
+    int d3 = c3 - '0';
+    int d4 = c4 - '0';
+    int d5 = c5 - '0';
+
+    if (d1 < 1 || d1 > 9 ||
+        d2 < 1 || d2 > 9 ||
+        d3 < 1 || d3 > 9 ||
+        d4 < 1 || d4 > 9 ||
+        d5 < 1 || d5 > 9) {
+        return 1;
+    }
+
+    putchar('0' + d3);
+    putchar('0' + d4);
+    putchar('0' + d5);
+    putchar('0' + d1);
+    putchar('0' + d2);
 
     return 0;
 }
